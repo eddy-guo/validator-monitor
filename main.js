8e9e1e9c5c599216f2274b607b752df04e80b498
unknown <eddyyanruguo@gmail.com>
2022-11-22 00:29:02 -0500

    feature - register script to avoid asnyc issue

--- main.js
+++ main.js
@@ -1,34 +1,10 @@
-require("dotenv").config();
-const { Client, GatewayIntentBits, REST, Routes } = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-
-const commands = [
-  {
-    name: "ping",
-    description: "Replies with Pong!",
-  },
-];
-
-const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
-
-(async () => {
-  try {
-    console.log("Started refreshing application (/) commands.");
-
-    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
-      body: commands,
-    });
-
-    console.log("Successfully reloaded application (/) commands.");
-  } catch (error) {
-    console.error(error);
-  }
-})();
 
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
