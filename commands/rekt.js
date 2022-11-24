1e6bba36852a4e1b37ebdb1f5eafc8a511d9c46c
unknown <eddyyanruguo@gmail.com>
2022-11-24 13:43:13 -0500

    feature - embed for rekt news, parse for most recent

--- commands/rekt.js
+++ commands/rekt.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
 const { request } = require("undici");
 const { parse } = require("node-html-parser");
 
@@ -9,8 +10,20 @@ module.exports = {
   async execute(interaction) {
     const response = await request("https://rekt.news/");
     const data = await response.body.text();
-    console.log(parse(data).querySelectorAll(".post-excerpt")[0].rawText.slice(0, -4));
-    console.log(parse(data).querySelectorAll(".post-excerpt")[1].rawText.slice(0, -4));
-    return interaction.reply("REKT RESPONSE");
+    const title = parse(data).querySelectorAll(".post-title")[0].firstChild.rawText;
+    const post = parse(data).querySelectorAll(".post-excerpt")[0].rawText.slice(0, -4);
+    const href = parse(data).querySelectorAll(".post-excerpt-more")[0].firstChild.attributes["href"];
+
+    const rektEmbed = new EmbedBuilder()
+    .setColor(0x000000)
+    .setTitle(`${title}`)
+    .setURL(`https://rekt.news${href}`)
+    .setDescription(`${post}`)
+    .setThumbnail("https://pbs.twimg.com/profile_images/1320861494112854018/7-zRzzum_400x400.jpg")
+    .setImage("https://pbs.twimg.com/profile_banners/1297925400090337280/1607454177/1500x500");
+
+    await interaction.deferReply();
+    await interaction.deleteReply();
+    await interaction.channel.send({ embeds: [rektEmbed] });
   },
 };
