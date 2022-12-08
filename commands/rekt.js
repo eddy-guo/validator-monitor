87ee55665925f0bd2ff006c369101fdaf7ad30e0
unknown <eddyyanruguo@gmail.com>
2022-12-08 07:51:37 -0500

    add embed logic to non-slash commands, add content to .send, finalize

--- commands/rekt.js
+++ commands/rekt.js
@@ -15,41 +15,37 @@ const redis = new Redis({
   password: `${process.env.REDIS_PASSWORD}`,
 });
 
-// // redis set function - only needed once to have the old/current title in place, never again
-// function setCache() {
-//   return new Promise((resolve) => {
-//     async () => redis.set("rektTitle", getRekt());
-//     resolve("Cached");
-//   });
-// }
-// setCache();
-
-// helper: return title
-async function getRekt() {
-  const response = await request("https://rekt.news/");
-  const data = await response.body.text();
-  const title =
-    parse(data).querySelectorAll(".post-title")[0].firstChild.structuredText;
-  return title;
-}
-
 // helper: check difference between cache and rekt api call
 async function getDifference() {
-  const currTitle = await getRekt();
+  // embed setup
+  const response = await request("https://rekt.news/");
+  const data = await response.body.text();
+  const title = parse(data).querySelectorAll(".post-title")[0].firstChild.structuredText;
+  const post = parse(data).querySelectorAll(".post-excerpt")[0].structuredText.slice(0, -4);
+  const href =parse(data).querySelectorAll(".post-excerpt-more")[0].firstChild.attributes["href"];
+
+  const rektEmbed = new EmbedBuilder()
+    .setColor(0x000000)
+    .setTitle(`${title}`)
+    .setURL(`https://rekt.news`)
+    .setDescription(`${post} \n [MORE](https://rekt.news${href})`)
+    .setThumbnail("https://pbs.twimg.com/profile_images/1320861494112854018/7-zRzzum_400x400.jpg")
+    .setImage("https://pbs.twimg.com/profile_banners/1297925400090337280/1607454177/1500x500")
+    .setTimestamp();
+    
+  // check difference
+  // remember that the previous title should already be cached, if not an extra article will send
+  const currTitle = title;
   const cachedTitle = await redis.get("rektTitle", (err, reply) => {
     if (err) throw err;
     return reply;
   });
   if (currTitle == cachedTitle) {
-    console.log(currTitle);
-    console.log(cachedTitle);
-    client.channels.cache.get("1046953428489883719").send("No Update");
-    console.log("No Update"); // make this a discord message (for testing)
+    console.log(`Current Title: ${currTitle} \nCached Title: ${cachedTitle} \nNo Update.`);
+    // client.channels.cache.get("1046953428489883719").send("No Update");
   } else {
-    console.log(currTitle);
-    console.log(cachedTitle);
-    client.channels.cache.get("1046953428489883719").send("Updated");
-    console.log("Updated"); // make this a discord message
+    console.log(`Current Title: ${currTitle} \nCached Title: ${cachedTitle} \nUpdated!`);
+    client.channels.cache.get("1046953428489883719").send({content: "**New Rekt article out now** :arrow_lower_left:", embeds: [rektEmbed] });
     redis.set("rektTitle", currTitle);
   }
 }
@@ -91,6 +87,6 @@ module.exports = {
       .setTimestamp();
 
     await interaction.deleteReply();
-    await interaction.channel.send({ embeds: [rektEmbed] });
+    await interaction.channel.send({content: "**The latest Rekt article** :arrow_lower_left:", embeds: [rektEmbed] });
   },
 };
